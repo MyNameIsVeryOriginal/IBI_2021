cb8bd09bbc62bf3b45e0c511da1dea634b4710ba
Sara345O <77624955+Sara345O@users.noreply.github.com>
2021-02-15 15:24:44 +0100

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -12,7 +12,7 @@ for ((i=2 ; $i<=4 ; i++))
 	#convertir nos fichiers de sam à bam:
     do samtools view -S -b ERR230025${i}.sam > ERR230025${i}.nontrie.bam
 	#trier nos fichiers .bam
-       samtools sort ERR230025${i}.nontrie.bam ERR230025${i}
+       samtools sort ERR230025${i}.nontrie.bam -o ERR230025${i}
 	#indexer nos fichiers 
        samtools index ERR230025${i}.bam
 done
